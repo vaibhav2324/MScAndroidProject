26c83da3c0817842caf96bca43bef56c40acf331
vaibhav <vaibhavdada125@gmail.com>
2020-03-30 21:35:45 +0530

    appointment activity created

--- UITesting/app/src/main/java/com/example/uitestingapplication/AppointmentUIActivity.java
+++ UITesting/app/src/main/java/com/example/uitestingapplication/AppointmentUIActivity.java
@@ -2,13 +2,70 @@ package com.example.uitestingapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
-public class AppointmentUIActivity extends AppCompatActivity {
+import java.util.Calendar;
+import java.util.Date;
 
+public class AppointmentUIActivity extends AppCompatActivity implements
+    DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener{
+    Button button;
+    TextView tvResult;
+    int day,month,year,hour,minute;
+    int finalDay,finalMonth,finalYear,finalHour,finalMinute;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_appointment_u_i);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        tvResult = (TextView)findViewById(R.id.tvResult);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            Calendar cal = Calendar.getInstance();
+            year = cal.get(Calendar.YEAR);
+            month = cal.get(Calendar.MONTH);
+            day = cal.get(Calendar.DAY_OF_MONTH);
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(AppointmentUIActivity.this, AppointmentUIActivity.this, year, month, day );
+            datePickerDialog.show();
+
+            }
+        });
+    }
+    @Override
+    public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+        finalYear = i;
+        finalMonth = i1;
+        finalDay = i2;
+
+        Calendar calendar = Calendar.getInstance();
+        hour = calendar.get(Calendar.HOUR_OF_DAY);
+        minute = calendar.get(Calendar.MINUTE);
+        TimePickerDialog timePickerDialog = new TimePickerDialog((AppointmentUIActivity.this), AppointmentUIActivity.this, hour,minute,false);
+        timePickerDialog.show();
+    }
+
+    @Override
+    public void onTimeSet(TimePicker timePicker, int i, int i1) {
+        finalHour = i;
+        finalMinute = i1;
+
+
+        tvResult.setText(" year"+finalYear+" / month"+finalMonth+" / day :"+finalDay+"\n"+"hour : "+finalHour+" minutes : "+finalMinute);
     }
 }

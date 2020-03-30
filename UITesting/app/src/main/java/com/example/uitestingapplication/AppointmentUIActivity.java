4bbe9a722fa7c15b3d28b41e075a2c73b2411540
vaibhavdada125@gmail.com <tureraosantosh@gmail.com>
2020-03-30 14:12:18 +0530

    updated

---
+++ UITesting/app/src/main/java/com/example/uitestingapplication/AppointmentUIActivity.java
@@ -0,0 +1,14 @@
+package com.example.uitestingapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AppointmentUIActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_appointment_u_i);
+    }
+}

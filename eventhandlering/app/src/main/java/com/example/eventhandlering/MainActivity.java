2990216e042b8a79462dc35f4ffff39ef5ae778d
vaibhavdada125@gmail.com <tureraosantosh@gmail.com>
2020-04-04 15:04:37 +0530

    changes in eventhandeling prj

--- eventhandlering/app/src/main/java/com/example/eventhandlering/MainActivity.java
+++ eventhandlering/app/src/main/java/com/example/eventhandlering/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.eventhandlering;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText edittext1, edittext2;
+    private Button buttonSum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonSum = (Button) findViewById(R.id.button);
+        buttonSum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"hello howare you ",Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+
     }
 }

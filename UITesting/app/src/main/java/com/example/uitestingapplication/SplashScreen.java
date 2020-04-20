215de659b95a79e0361836df5b84a0e35b7ef317
vaibhavdada125@gmail.com <tureraosantosh@gmail.com>
2020-04-20 18:14:24 +0530

    splash screen added

---
+++ UITesting/app/src/main/java/com/example/uitestingapplication/SplashScreen.java
@@ -0,0 +1,17 @@
+package com.example.uitestingapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
+
+        Intent intent = new Intent(this,DashboardUIActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
